3239764fccf5b981144671d6ba314fd02be1cfef
Winston Chang <winston@stdout.org>
2013-05-15 15:11:50 -0500

    Add docs

--- R/gridster.r
+++ R/gridster.r
@@ -1,10 +1,30 @@
-#' Create a gridster frame on a Shiny web page
+#' Create a Gridster frame on a Shiny web page
 #'
-#' 
+#' @param marginx Horizontal margin between each grid item, in pixels.
+#' @param marginy Vertical margin between each grid item, in pixels.
+#' @param width Width of each tile, in pixels.
+#' @param height Height of each tile, in pixels.
+#' @param ... Other properties or elements to include.
+#'
+#' @seealso \code{\link{gridsterItem}}
+#'
 #' @examples
+#' \dontrun{
+#' shinyUI(bootstrapPage(
 #'
-#'
-#'
+#'  gridster(width = 200, height = 200,
+#'    gridsterItem(col = 1, row = 1, sizex = 1, sizey = 1,
+#'      sliderInput("n", "Input value:", min = 0, max = 50, value = 10)
+#'    ),
+#'    gridsterItem(col = 2, row = 1, sizex = 1, sizey = 1,
+#'      textOutput("myText")
+#'    ),
+#'    gridsterItem(col = 1, row = 2, sizex = 2, sizey = 1,
+#'      plotOutput("myPlot", height = 200)
+#'    )
+#'  )
+#' )
+#' }
 #' @export
 gridster <- function(..., marginx = 16, marginy = 16, width = 140, height = 140) {
   addResourcePath(
@@ -35,7 +55,16 @@ gridster <- function(..., marginx = 16, marginy = 16, width = 140, height = 140)
   )
 }
 
-
+#' Add a Gridster item within a Gridster frame to a Shiny web page
+#'
+#' @param col The column in which to put the item.
+#' @param row The row in which to put the item.
+#' @param sizex Width, in number of tiles.
+#' @param sizey Height, in number of tiles.
+#' @param ... Other properties or elements to include.
+#'
+#' @seealso \code{\link{gridster}}
+#'
 #' @export
 gridsterItem <- function(..., col = NULL, row = NULL, sizex = NULL, sizey = NULL) {
   tags$li(`data-col` = col,
